e60c74d01bb5cd97c7b22825e8721fd60eedf46e
Edita Keškić <edita.keskic@gmail.com>
2021-06-10 14:01:06 +0200

    Removing Modal and Backdrop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import Todo from './components/Todo';  
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
- 
- function App() {
+import Todo from "./components/Todo";
+
+function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text='Learn React' />
-      <Todo text='Master React' />
-      <Todo text='Explore the full React course' />
-      <Modal />
-      <Backdrop />
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
