bd33906b22f2cc72e7d7b060dbc27c966df4736f
Edita Keškić <edita.keskic@gmail.com>
2021-06-10 14:01:47 +0200

    Creating Modal and Backdrop states

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
   function deleteHandler() {
-    console.log('Clicked!');
-    console.log(props.text);
+    setModalIsOpen(true);
   }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler()}>
+        <button className="btn" onClick={deleteHandler}>
           Delete
         </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
