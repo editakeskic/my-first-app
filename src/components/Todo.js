5149c982e6dc079a29aa23799086a650ef2a01e7
Edita Keškić <edita.keskic@gmail.com>
2021-06-11 23:40:22 +0200

    Close Modal by Backdrop click

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,6 +10,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler () {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -19,7 +23,7 @@ function Todo(props) {
         </button>
       </div>
       {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
